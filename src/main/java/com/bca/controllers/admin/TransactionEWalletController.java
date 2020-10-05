0caa7e838f0969bd1e241cdf2aa9775218b2e9f4
Herdhiantoko Fathani <herdhiantokofathani@gmail.com>
2020-10-05 13:40:53 +0700

    :rocket: [Add] Controllers for Admin

---
+++ src/main/java/com/bca/controllers/admin/TransactionEWalletController.java
@@ -0,0 +1,51 @@
+package com.bca.controllers.admin;
+
+import com.bca.dto.TransactionEWalletForm;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/transaction_ewallet")
+public class TransactionEWalletController {
+  private String BASE_PATH = "/admin/transaction_ewallet";
+
+  @GetMapping
+  public String index() {
+    return BASE_PATH.concat("/index");
+  }
+
+  @GetMapping("/create")
+  public String create() {
+    return BASE_PATH.concat("/form");
+  }
+
+  @PostMapping("/create")
+  public String insert() {
+    return "redirect:".concat(BASE_PATH);
+  }
+
+  @GetMapping("/edit/{id}")
+  public String edit(@PathVariable("id") long id, Model model) {
+    // TODO: add get by service
+    return BASE_PATH.concat("/form");
+  }
+
+  @PutMapping("/edit/{id}")
+  public String update(@PathVariable("id") long id, TransactionEWalletForm form) {
+    // TODO: add update service
+    return "redirect:".concat(BASE_PATH);
+  }
+
+  @DeleteMapping("/remove/{id}")
+  public String delete(@PathVariable("id") long id) {
+    // TODO: add delete service
+    return "redirect:".concat(BASE_PATH);
+  }
+}
